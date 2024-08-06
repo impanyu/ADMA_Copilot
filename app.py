f0bdf9d936faa527813f5c320dcca4cffe5e3698
impanyu <impanyu@gmail.com>
2024-08-06 04:54:49 -0500

    test

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ class CustomPromptTemplate(StringPromptTemplate):
 
 
 def define_agent():
-    llm=ChatOpenAI(temperature=0,model="gpt-4o")
+    llm=ChatOpenAI(temperature=0,model="gpt-4-turbo")
     search = SerpAPIWrapper()
 
 
