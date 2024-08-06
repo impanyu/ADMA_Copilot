27ef069693f1036b057ca866343eda8fe6cca3c0
impanyu <impanyu@gmail.com>
2024-08-06 07:27:08 -0500

    test

--- ADMA_Tools.py
+++ ADMA_Tools.py
@@ -71,7 +71,7 @@ class ADMA_check_file_input_schema(BaseModel):
 
 @tool("ADMA_check_file", args_schema=ADMA_check_file_input_schema)
 def ADMA_check_file(dir_path):
-    """Always call this tool when the user want to check the content of dir_path on the ADMA server."""
+    """Always call this tool when the user want to check or display the content of dir_path on the ADMA server."""
     download_url = f"{root_url}/api/download/?target_path={dir_path}"
     response = requests.get(download_url)
 
